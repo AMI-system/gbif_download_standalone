8eeb0a5186e0a51329c90b4ce8057670e95dbd92
Levan Bokeria <levan.bokeria@gmail.com>
2023-10-26 12:27:02 +0100

    minor

--- 03_download_images/fetch_gbif_moth_data.sh
+++ 03_download_images/fetch_gbif_moth_data.sh
@@ -25,7 +25,7 @@ python fetch_gbif_moth_data.py \
 --write_directory "/bask/homes/r/rybf4168/vjgo8416-amber/data/gbif_download_standalone/gbif_images/" \
 --occ_files "/bask/homes/r/rybf4168/vjgo8416-amber/data/gbif_download_standalone/dwca_preprocessed/occurrence_dataframes_20231018/" \
 --media_file "/bask/homes/r/rybf4168/vjgo8416-amber/data/gbif_download_standalone/dwca_preprocessed/multimedia_lepidoptera_20231018.csv" \
---species_checklist "/bask/homes/r/rybf4168/vjgo8416-amber/projects/gbif_download_standalone/species_checklists/uksi-moths-keys-nodup.csv" \
+--species_checklist "/bask/homes/r/rybf4168/vjgo8416-amber/projects/gbif_download_standalone/species_checklists/singapore-moths-keys-nodup.csv" \
 --use_parallel True \
 --use_multiproc False \
 --max_data_sp 1000 \
