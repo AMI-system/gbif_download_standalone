a8bb929249e86b8dc9f4ca2cecb54ba3ae599208
Levan Bokeria <levan.bokeria@gmail.com>
2023-11-02 10:03:55 +0000

    minor

--- 03_download_images/fetch_images_split_dwca.sh
+++ 03_download_images/fetch_images_split_dwca.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #SBATCH --account vjgo8416-amber
 #SBATCH --qos turing
-#SBATCH --time 20:00:00
+#SBATCH --time 04:00:00
 #SBATCH --nodes 1
 #SBATCH --cpus-per-task 36
-#SBATCH --mem=200G
+#SBATCH --mem=100G
+#SBATCH --output=download.out
 
 # Module loading
 module purge # unloads and loaded modules and resets the environment
@@ -25,7 +26,7 @@ python fetch_images_split_dwca.py \
 --write_directory "/bask/homes/r/rybf4168/vjgo8416-amber/data/gbif_download_standalone/gbif_images/" \
 --occ_files "/bask/homes/r/rybf4168/vjgo8416-amber/data/gbif_download_standalone/dwca_preprocessed/occurrence_dataframes_20231018/" \
 --media_file "/bask/homes/r/rybf4168/vjgo8416-amber/data/gbif_download_standalone/dwca_preprocessed/multimedia_lepidoptera_20231018.csv" \
---species_checklist "/bask/homes/r/rybf4168/vjgo8416-amber/projects/gbif_download_standalone/species_checklists/singapore-moths-keys-nodup.csv" \
+--species_checklist "/bask/homes/r/rybf4168/vjgo8416-amber/projects/gbif_download_standalone/species_checklists/uksi-moths-keys-nodup.csv" \
 --use_parallel True \
 --use_multiproc False \
 --max_data_sp 1000 \
