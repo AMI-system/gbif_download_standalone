db6a162a0c536e69ab789c84cb30796bb3a25307
KatrionaGoldmann <kgoldmann@turing.ac.uk>
2024-11-01 10:04:58 +0000

    update for miniforge

--- 03_download_images/fetch_images_split_dwca.sh
+++ 03_download_images/fetch_images_split_dwca.sh
@@ -8,25 +8,24 @@
 #SBATCH --output=download_thailand.out
 
 # Module loading
-module purge # unloads and loaded modules and resets the environment
-module load baskerville # loads the default Baskerville environment
-module load bask-apps/live # load the stable, default application stack
-module load Miniconda3/4.10.3 # load the Conda package manager
+module purge
+module load baskerville
+module load bask-apps/live
+module load Miniforge3/24.1.2-0
+eval "$(${EBROOTMINIFORGE3}/bin/conda shell.bash hook)"
+source "${EBROOTMINIFORGE3}/etc/profile.d/mamba.sh"
 
-set -e # exit immediately if there is an error
-eval "$(${EBROOTMINICONDA3}/bin/conda shell.bash hook)" # initialise Conda
+# Define the path to your existing Conda environment (modify as appropriate)
+CONDA_ENV_PATH="/bask/projects/v/vjgo8416-amber/conda_envs/gbif_download_standalone"
 
-export CONDA_ENV_PATH="/bask/projects/v/vjgo8416-amber/conda_envs/gbif_download_standalone"
-
-# Activate the environment
-conda activate "${CONDA_ENV_PATH}"
+mamba activate "${CONDA_ENV_PATH}"
 
 # Execute your python programme
 python fetch_images_split_dwca.py \
     --write_directory "/bask/projects/v/vjgo8416-amber/data/gbif_download_standalone/gbif_images/" \
     --occ_files "/bask/projects/v/vjgo8416-amber/data/gbif_download_standalone/dwca_preprocessed/occurrence_dataframes_20231018/" \
     --media_file "/bask/projects/v/vjgo8416-amber/data/gbif_download_standalone/dwca_preprocessed/multimedia_lepidoptera_20231018.csv" \
-    --species_checklist "/bask/homes/f/fspo1218/amber/projects/gbif_download_standalone/species_checklists/thailand-moths-keys-nodup.csv" \
+    --species_checklist "/bask/homes/f/fspo1218/amber/projects/gbif_download_standalone/species_checklists/kenya-uganda-moths-keys-nodup.csv" \
     --max_data_sp 1000 \
     --skip_non_adults
 
