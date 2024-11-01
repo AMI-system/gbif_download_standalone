fb2432d3cbd8ee6e9875a67ba086ddbd8ff7db7d
KatrionaGoldmann <kgoldmann@turing.ac.uk>
2024-11-01 10:06:05 +0000

    update for miniforge

--- 01_prepare_checklist/fetch_taxon_keys.sh
+++ 01_prepare_checklist/fetch_taxon_keys.sh
@@ -10,10 +10,14 @@
 module purge # unloads and loaded modules and resets the environment
 module load baskerville # loads the default Baskerville environment
 module load bask-apps/live # load the stable, default application stack
-module load Miniconda3/4.10.3 # load the Conda package manager
+# module load Miniconda3/4.10.3 # load the Conda package manager
+module load Miniforge3/24.1.2-0
+
+eval "$(${EBROOTMINIFORGE3}/bin/conda shell.bash hook)"
+source "${EBROOTMINIFORGE3}/etc/profile.d/mamba.sh"
 
 set -e # exit immediately if there is an error
-eval "$(${EBROOTMINICONDA3}/bin/conda shell.bash hook)" # initialise Conda
+# eval "$(${EBROOTMINICONDA3}/bin/conda shell.bash hook)" # initialise Conda
 
 export CONDA_ENV_PATH="/bask/projects/v/vjgo8416-amber/conda_envs/gbif_download_standalone"
 
@@ -27,5 +31,5 @@ python fetch_taxon_keys.py \
 --column_name_species species_name_provided \
 --column_name_authority authority_name_provided \
 --output_filepath ../species_checklists/costarica-moths-keys.csv \
---place London19Oct2023 \
+--place London01Oct2024 \
 --use_multithreading True
